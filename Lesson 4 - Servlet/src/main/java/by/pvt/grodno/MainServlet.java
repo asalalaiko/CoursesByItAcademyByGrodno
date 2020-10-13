211f9e5f7e99ae07846ac3898dad43cf90da283f
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2020-10-13 17:25:19 +0300

    Add files via upload

--- Lesson 4 - Servlet/src/main/java/by/pvt/grodno/MainServlet.java
+++ Lesson 4 - Servlet/src/main/java/by/pvt/grodno/MainServlet.java
@@ -6,11 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+        writer.write("Hello word!");
+
+        super.doGet(req, resp);
     }
 
     @Override
