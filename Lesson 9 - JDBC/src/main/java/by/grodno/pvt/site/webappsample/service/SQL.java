50d781101cd8cb2c5008b79e4acf8c46f2bb56cd
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2020-11-06 22:24:40 +0300

    Add files via upload

--- Lesson 9 - JDBC/src/main/java/by/grodno/pvt/site/webappsample/service/SQL.java
+++ Lesson 9 - JDBC/src/main/java/by/grodno/pvt/site/webappsample/service/SQL.java
@@ -3,7 +3,7 @@ package by.grodno.pvt.site.webappsample.service;
 public class SQL {
 
 	public static final String SELECT_ALL = "select * from public.testtable order by id";
-	public static final String SELECT_ALL_AND_DEP = "SELECT * FROM public.testtable t FULL JOIN public.dept d ON t.dept_num = d.id order by t.id";
+	public static final String SELECT_ALL_AND_DEP = "SELECT * FROM public.testtable t LEFT OUTER JOIN public.dept d ON t.dept_num = d.id order by t.id";
 	public static final String SELECT_BY_ID = "select * from public.testtable where id = ?";
 	public static final String SELECT_DEP_BY_ID = "select * from public.dept where id = ?";
 	public static final String DELETE_BY_ID = "delete from public.testtable where id = ?";
