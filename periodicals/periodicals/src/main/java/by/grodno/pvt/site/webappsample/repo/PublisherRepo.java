7f400238bd2dca1f48452e9131659d25f0b3c924
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2020-12-17 01:36:33 +0300

    Delete PublisherRepo.java

--- periodicals/periodicals/src/main/java/by/grodno/pvt/site/webappsample/repo/PublisherRepo.java
+++
@@ -1,7 +0,0 @@
-package by.grodno.pvt.site.webappsample.repo;
-
-import by.grodno.pvt.site.webappsample.domain.Publisher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PublisherRepo extends JpaRepository <Publisher, Integer> {
-}
