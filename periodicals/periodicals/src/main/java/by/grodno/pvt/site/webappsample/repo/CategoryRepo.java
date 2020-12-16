0ed6687bd3efb37805dfee0ced75eb7884d51cde
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2020-12-17 01:36:12 +0300

    Delete CategoryRepo.java

--- periodicals/periodicals/src/main/java/by/grodno/pvt/site/webappsample/repo/CategoryRepo.java
+++
@@ -1,7 +0,0 @@
-package by.grodno.pvt.site.webappsample.repo;
-
-import by.grodno.pvt.site.webappsample.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepo extends JpaRepository<Category, Integer> {
-}
