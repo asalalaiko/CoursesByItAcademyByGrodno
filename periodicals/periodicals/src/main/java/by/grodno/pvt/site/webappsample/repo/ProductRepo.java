f2028e44fd4b2935a4f923bed70597da37b090eb
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2020-12-17 01:36:19 +0300

    Delete ProductRepo.java

--- periodicals/periodicals/src/main/java/by/grodno/pvt/site/webappsample/repo/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package by.grodno.pvt.site.webappsample.repo;
-
-import by.grodno.pvt.site.webappsample.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product, Integer> {
-}
