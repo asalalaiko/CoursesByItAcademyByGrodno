a0c7f8d979949f72ab889a8f8bad5318eb48eab8
asalalaiko <57458353+asalalaiko@users.noreply.github.com>
2021-01-17 23:55:18 +0300

    17/01/21-3

--- periodicals/src/main/java/by/grodno/pvt/site/webappsample/config/SecurityConfiguration.java
+++ periodicals/src/main/java/by/grodno/pvt/site/webappsample/config/SecurityConfiguration.java
@@ -1,7 +1,11 @@
 package by.grodno.pvt.site.webappsample.config;
 
+import by.grodno.pvt.site.webappsample.repo.UserRepo;
+import by.grodno.pvt.site.webappsample.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,10 +13,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
+	@Autowired
+	private UserService userService;
+
+
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -24,7 +33,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
 		.formLogin()
 			.loginPage("/login")
 			.permitAll()
-			.defaultSuccessUrl("/users")
+			.defaultSuccessUrl("/")
 			.and()
 		.logout()
 			.permitAll();
@@ -35,5 +44,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
 	public PasswordEncoder noop() {
 		return NoOpPasswordEncoder.getInstance();
 	}
+//	@Bean
+//	@Override
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user =
+//				User.withDefaultPasswordEncoder()
+//						.username("a")
+//						.password("a")
+//						.roles("ADMIN")
+//						.build();
+//
+//		return new InMemoryUserDetailsManager(user);
+//	}
+
 	
 }
--- periodicals/src/main/java/by/grodno/pvt/site/webappsample/domain/User.java
+++ periodicals/src/main/java/by/grodno/pvt/site/webappsample/domain/User.java
@@ -24,6 +24,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    private Boolean active;
 
     @Column(name = "user_role")
     @Enumerated(EnumType.STRING)
