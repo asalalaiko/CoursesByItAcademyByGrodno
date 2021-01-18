a2b4f67f6e4d58d58b9a5c20fa8200c25df00747
asalalaiko <a.salalaiko@gmail.com>
2021-01-18 10:01:24 +0300

    18/01/21

---
+++ periodicals/src/main/java/by/grodno/pvt/site/webappsample/controller/RegistrationController.java
@@ -0,0 +1,52 @@
+package by.grodno.pvt.site.webappsample.controller;
+
+
+import by.grodno.pvt.site.webappsample.domain.User;
+import by.grodno.pvt.site.webappsample.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/activate/{id}")
+    String activation(@PathVariable Integer id) {
+
+   //    service.activateUser(id);
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/register")
+    String getForm(User user, Model model) {
+        if (user == null) {
+            user = new User();
+        }
+        model.addAttribute("user", user);
+        return "registerView";
+    }
+
+
+    @PostMapping("/register/new")
+    String registerPage(@Valid User user, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("User", user);
+            return "registerView";
+        }
+
+   //     service.saveUser(convertionService.convert(user, User.class));
+
+        return "redirect:/login";
+    }
+
+}
