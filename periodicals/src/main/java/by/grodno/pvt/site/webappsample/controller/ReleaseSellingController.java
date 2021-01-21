7cf47665283b30537c7b6b1124e4e95cf3cde1ec
asalalaiko <a.salalaiko@gmail.com>
2021-01-21 16:38:08 +0300

    21/01/21

---
+++ periodicals/src/main/java/by/grodno/pvt/site/webappsample/controller/ReleaseSellingController.java
@@ -0,0 +1,61 @@
+package by.grodno.pvt.site.webappsample.controller;
+
+
+import by.grodno.pvt.site.webappsample.domain.Release;
+import by.grodno.pvt.site.webappsample.repo.ReleaseRepo;
+import by.grodno.pvt.site.webappsample.service.ReleaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ReleaseSellingController {
+
+    @Autowired
+    private ReleaseService releaseService;
+    @Autowired
+    private ConversionService convertionService;
+
+
+    private List<Release> getSoldReleases(HttpSession session) {
+        return (List<Release>) session.getAttribute("selectRelease");
+    }
+
+    @GetMapping("/release/buy")
+    public String editUserForm(@RequestParam(value="id") Integer id, HttpSession session) {
+
+        List<Release> attribute = getSoldReleases(session);
+
+        if (attribute == null) {
+            session.setAttribute("selectRelease", new ArrayList<Release>());
+        }
+        getSoldReleases(session).add(convertionService.convert(releaseService.getRelease(id), Release.class));
+
+   //     getSoldReleases(session).add(releaseService.getRelease(id), Release.class);
+
+
+        return "redirect:?id="+id;
+    }
+
+    @GetMapping("/sold")
+    public String sold(Model model, HttpSession session) {
+
+        List<Release> attribute = getSoldReleases(session);
+
+        if (attribute == null) {
+            session.setAttribute("selectRelease", new ArrayList<Release>());
+        }
+
+        model.addAttribute("release", getSoldReleases(session));
+
+        return "getSoldReleases";
+    }
+}
